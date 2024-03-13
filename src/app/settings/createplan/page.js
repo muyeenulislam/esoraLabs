e74f0dc5fedaca321e936e34b8ba9e372aa986b2
muyeen <muyeenulislamsakif@gmail.com>
2024-03-13 19:53:30 +0600

    settings

--- src/app/settings/createplan/page.js
+++ src/app/settings/createplan/page.js
@@ -3,17 +3,20 @@
 import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import Image from "next/image";
-import { Radio } from "antd";
+import { Radio, Modal } from "antd";
 
 import PageHeading from "@/components/pageheading/pageheading";
 import Spacer from "@/components/spacer/spacer";
 import DefaultInput from "@/components/inputs/defaultinput";
+import WhiteButton from "@/components/buttons/whitebutton";
+import YellowButton from "@/components/buttons/yellowbutton";
 
 import CurrencyDropdownInput from "@/components/inputs/currencydropdowninput";
 
 const CreatePlan = () => {
   const router = useRouter();
 
+  const [isOpen, setIsOpen] = useState(false);
   const [state, setState] = useState({
     name: "",
     subtitle: "",
@@ -22,8 +25,36 @@ const CreatePlan = () => {
     currency: "USD",
     currencySymbol: "$",
     subHeading: "",
+    features: ["", "", ""],
+    badge: "",
   });
-  console.log(state);
+
+  const handleAddMorefeatures = () => {
+    setState({
+      ...state,
+      features: [...state.features, ""],
+    });
+  };
+
+  const handleRemovefeatures = (index) => {
+    let array = [...state.features];
+    array.splice(index, 1);
+    setState({
+      ...state,
+      features: array,
+    });
+  };
+
+  const handleSubmit = () => {
+    console.log(state);
+    setIsOpen(true);
+  };
+
+  const handleMakeActive = () => {
+    console.log("make active");
+    router.push("/settings?tab=4");
+  };
+
   return (
     <div>
       <div className="flex justify-between items-center">
@@ -122,7 +153,134 @@ const CreatePlan = () => {
             onChange={(e) => setState({ ...state, subHeading: e.target.value })}
           />
         </div>
+        {/* whats included */}
+        <div className="p-6 border-b border-grayBorderDashboard">
+          <h4 className="text-[16px] text-primary font-bold m-0">
+            What&apos;s Included
+          </h4>
+          <Spacer height="8px" />
+          <p className="text-[14px] text-subtitleText font-normal m-0">
+            Enter all the information as points in each field and add more
+            fields as you need
+          </p>
+          <Spacer height="16px" />
+          {state?.features?.map((item, index) => (
+            <>
+              {index < 3 ? (
+                <DefaultInput
+                  onChange={(e) => {
+                    let newValue = [...state.features];
+                    newValue[index] = e.target.value;
+                    setState({
+                      ...state,
+                      features: newValue,
+                    });
+                  }}
+                  key={index}
+                />
+              ) : (
+                <div className="flex gap-3" key={index}>
+                  <DefaultInput
+                    onChange={(e) => {
+                      let newValue = [...state.features];
+                      newValue[index] = e.target.value;
+                      setState({
+                        ...state,
+                        features: newValue,
+                      });
+                    }}
+                  />
+                  <Image
+                    onClick={() => {
+                      handleRemovefeatures(index);
+                    }}
+                    src={"/images/black-cross.svg"}
+                    height={20}
+                    width={20}
+                    alt=""
+                  />
+                </div>
+              )}
+              <Spacer height="12px" />
+            </>
+          ))}
+          <Spacer height="16px" />
+          <div className="flex justify-center items-center gap-2">
+            <Image
+              src="/images/plus-icon.svg"
+              height={20}
+              width={20}
+              alt=""
+              onClick={handleAddMorefeatures}
+            />
+            <p
+              className="text-[16px] text-subtitleText font-medium m-0"
+              onClick={handleAddMorefeatures}
+            >
+              Add another field
+            </p>
+          </div>
+        </div>
+        {/* badge */}
+        <div className="p-6 border-b border-grayBorderDashboard">
+          <h4 className="text-[16px] text-primary font-bold m-0">Badge</h4>
+          <Spacer height="8px" />
+          <p className="text-[14px] text-subtitleText font-normal m-0">
+            Badge can help to make different this plan from other plans
+          </p>
+          <Spacer height="16px" />
+          <DefaultInput
+            onChange={(e) => setState({ ...state, badge: e.target.value })}
+          />
+        </div>
+        {/* button */}
+        <div className="p-6 flex justify-between items-center">
+          <WhiteButton
+            text="Cancel"
+            onClick={() => router.push("/settings?tab=4")}
+          />
+          <YellowButton text="Save Changes" onClick={handleSubmit} />
+        </div>
       </div>
+      {isOpen && (
+        <Modal
+          open={isOpen}
+          centered={true}
+          onCancel={() => setIsOpen(false)}
+          closeIcon={false}
+          footer={false}
+        >
+          <div>
+            <div className="h-12 w-12 rounded-full bg-primary flex justify-center items-center border-8 border-[#EAEDF4] ">
+              <Image
+                src="/images/greentick.svg"
+                height={24}
+                width={24}
+                alt=""
+              />
+            </div>
+            <Spacer height="20px" />
+            <h1 className="headers text-primary text-[20px] mb-0">
+              Your plan was created successfully
+            </h1>
+
+            <p className="font-normal text-[14px] text-subtitleText mb-0">
+              Do you want to make this plan active now?
+            </p>
+            <Spacer height="32px" />
+            <div className="grid grid-cols-2 gap-3" key={1}>
+              <WhiteButton
+                text={"No, I’ll do it later"}
+                onClick={() => router.push("/settings?tab=4")}
+              />
+              <YellowButton
+                text={"Yes, make it Active"}
+                onClick={handleMakeActive}
+              />
+            </div>
+          </div>
+        </Modal>
+      )}
     </div>
   );
 };
