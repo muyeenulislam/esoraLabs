8eac3a3fe99d5ddd02d13871c496ddc270404214
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2024-03-15 14:40:16 +0600

    done

--- src/app/clients/[name]/projects/[website]/website_overview.js
+++ src/app/clients/[name]/projects/[website]/website_overview.js
@@ -1,9 +1,13 @@
+import YellowButton from "@/components/buttons/yellowbutton";
 import React from "react";
 import { FiMessageSquare, FiPhone } from "react-icons/fi";
 
 const Website_overview = ({ data }) => {
+
+  console.log("data", data);
+  
   return (
-    <div className="border h-auto mx-[200px] mt-[32px] rounded-2xl shadow-md">
+    <div className="border h-auto mx-[200px] mt-[24px] rounded-2xl shadow-md">
       <div className=" p-[24px] bg-[#0B132B] rounded-t-2xl"></div>
       <div className="grid grid-cols-2 justify-between  bg-[#0B132B] ">
         <div className="px-[24px] pb-[24px] text-[#FFFFFF]">
@@ -14,10 +18,10 @@ const Website_overview = ({ data }) => {
 
       <div className="grid grid-cols-2 pb-[24px] justify-between bg-gray-100">
         <div className="px-[24px] pt-[24px] pb-[12px] text-[20px] font-bold">
-          <div className="text-[#0B132B] font-sans pb-[12px] text-base font-normal leading-5">
+          <div className="text-black font-sans pb-[12px] text-base font-normal leading-5">
             Industry type
           </div>
-          <div className="truncate text-blue-900 font-sans text-xl font-bold leading-6">
+          <div className="truncate text-black font-sans text-xl font-bold leading-6">
           Transportatione.
           </div>
         </div>
@@ -25,7 +29,7 @@ const Website_overview = ({ data }) => {
           <div className="text-[#0B132B] font-sans pb-[12px] text-base font-normal leading-5">
           Created On
           </div>{" "}
-          <div className="truncate text-blue-900 font-sans text-xl font-bold leading-6">
+          <div className="truncate text-black font-sans text-xl font-bold leading-6">
           01/27/2023
           </div>
         </div>
@@ -48,6 +52,23 @@ const Website_overview = ({ data }) => {
 
 </div>
       </div>
+      {/* {data && ( */}
+      <div className="flex justify-between rounded-b-lg justify-between  bg-[#0B132B] ">
+        <div className="px-[24px] pb-[24px] text-[#FFFFFF]">
+          <div className="text-[24px] mt-5 font-bold">Deliverables</div>
+          <div className="text-[16px] font-normal">3 revisions remaining</div>
+        </div>
+        <div className="p-5 mt-2">
+        <YellowButton
+                  imagealign="left"
+                  image={"/images/upload-cloud.svg"}
+                  text={"Upload"}
+                  
+                />
+        </div>
+      </div>
+
+      {/* )} */}
     </div>
   );
 };
